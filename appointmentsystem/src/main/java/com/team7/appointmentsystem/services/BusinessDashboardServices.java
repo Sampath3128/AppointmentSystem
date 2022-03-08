51e9bd6047f092ff1cbf07a7e5c242956af92165
Balkrishan Sehra <42343273+Renon133@users.noreply.github.com>
2022-03-08 11:16:27 +0530

    Changes in Business Images in Dashboard

--- appointmentsystem/src/main/java/com/team7/appointmentsystem/services/BusinessDashboardServices.java
+++ appointmentsystem/src/main/java/com/team7/appointmentsystem/services/BusinessDashboardServices.java
@@ -211,22 +211,25 @@ public class BusinessDashboardServices {
     {
         String profileImg = StringUtils.cleanPath(multipartFile.getOriginalFilename());
         Business business = businessRepository.findByBusinessId(businessId);
-        business.setBusinessImages();
-        Business savedBusiness = businessRepository.save(business);
-        String uploadDir = "./profile-image/" + savedBusiness.getBusinessid();
+        String uploadDir = "./business-image/" + businessId;
         Path uploadPath = Paths.get(uploadDir);
         if(!Files.exists(uploadPath)) {
             Files.createDirectories(uploadPath);
         }
         try (InputStream inputStream = multipartFile.getInputStream()) {
             Path filePath = uploadPath.resolve(profileImg);
-            System.out.println(filePath.toFile().getAbsolutePath().toString()) ;
+            BusinessImages businessImages = new BusinessImages(business,  filePath.toFile().getAbsolutePath());
+            ArrayList<BusinessImages> listOfImages = new ArrayList<>(List.of(businessImages));
+            business.setBusinessImages(listOfImages);
+            Business savedBusiness = businessRepository.save(business);
             Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
         }catch (IOException e) {
             throw  new IOException("Could not save uploaded file:" + profileImg);
         }
         return ResponseEntity.ok("File uploaded Successfully");
     }
+
+
 
 
 
